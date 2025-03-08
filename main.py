d80dff8053e141e4f58b9395806468efa38f4036
Jsshears0 <jsshears2@gmail.com>
2025-03-07 23:52:04 -0500

    Completed project testing

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 def main():
+    import sys
     from stats import get_book_text
     from stats import get_num_words
     from stats import get_num_characters
     from stats import char_dict_sort
 
-    text = get_book_text(path_to_file = "books/frankenstein.txt")
+    if len(sys.argv) !=2:
+        print(f"Usage: python3 main.py <path_to_file>")
+        sys.exit(1)
+    
+
+    path_to_file = sys.argv[1]
+    text = get_book_text(path_to_file)
     word_count = get_num_words(text)
     char_count = get_num_characters(text)
     sort_char = char_dict_sort(char_count)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path_to_file}")
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
